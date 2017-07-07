4d4f4759a00001136c502d03c635b1bb3eba9713
rcowles3 <ryan.cowles3@gmail.com>
2017-07-07 01:00:07 -0700

    customer view finished and working.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,9 +12,9 @@ var inquirer = require('inquirer');
 var connection = mysql.createConnection({
     host: 'localhost', // update with host
     port: 3306, // update with port
-    user: 'root', // update with user id
-    password: 'Fdd4e!i$f$', // update with user password
-    database: 'BAMAZON' // update with created database
+    user: '', // update with user id
+    password: '', // update with user password
+    database: '' // update with created database
 });
 
 // connecting to db, and displaying connection id
@@ -29,7 +29,7 @@ connection.connect(function(err) {
     displayProducts();
 
     // terminate sql connection
-    terminateConnection();
+    // terminateConnection();
 });
 
 // FUNCTIONS TO QUERY SQL DATABASE
@@ -58,6 +58,66 @@ var displayProducts = function() {
             console.log("ID:", res[i].ITEM_ID, "\nCAR:", res[i].PRODUCT_NAME, "\nPRICE: $", res[i].PRICE);
             console.log("\n------------------------------\n");
         }
+
+        // prompt user about purchases
+        promptCustomer();
+    })
+}
+
+// function to prompt user what they would like to buy
+var promptCustomer = function() {
+
+    // prompt user what they would like to purchase
+    inquirer.prompt([{
+        name: "buyingID",
+        message: "What is the ID of the vehicle that you would like to purchase?",
+        type: "input",
+    }, {
+        name: "buyingQuanity",
+        message: "How many vehicles would you like to purchase?",
+        type: "input"
+    }]).then(function(customerResponse) {
+
+        // SQL query
+        let sqlQuery = "SELECT * FROM PRODUCTS WHERE ITEM_ID = ?";
+
+        // selecting data from our database
+        connection.query(sqlQuery, [customerResponse.buyingID], function(err, res) {
+
+            // err catcher
+            if (err) throw err;
+
+            // var to hold buying price
+            let buyingPriceTotal = res[0].PRICE;
+            
+            // if else to check if stock available
+            if (customerResponse.buyingQuanity < res[0].STOCK_QUANITY) { // fulfill order
+
+                // var to hold sql query
+                let sqlQuery = "UPDATE PRODUCTS SET STOCK_QUANITY = STOCK_QUANITY - ? WHERE ITEM_ID = ?";
+
+                // query to update our database
+                connection.query(sqlQuery, [customerResponse.buyingQuanity, customerResponse.buyingID], function(err, res) {
+
+                    // err catcher
+                    if (err) throw err;
+
+                    // provide customer with order details
+                    console.log("\nThank you, your order has been submitted successfuly. \n\nYour total that will be billed is: $", buyingPriceTotal * customerResponse.buyingQuanity);
+                })
+
+                // terminate the sql connection
+                terminateConnection();
+            }
+            else { 
+
+                // cancel order
+                console.log("Sorry, there is an insufficient quantity of that vehicle in stock right now, please try again at a later time, or select another vehicle to purchase.");
+
+                // ask customer to choose another vehicle, or quit application.
+                promptCustomer();
+            }
+        })
     })
 }
 
